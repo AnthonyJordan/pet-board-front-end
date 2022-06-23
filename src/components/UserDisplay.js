9adcad24d9615ff2274a6255f5a62795cd6902a8
Anthony <Kain235@gmail.com>
2022-06-23 09:57:58 -0400

    Fixed pet load crash

--- src/components/UserDisplay.js
+++ src/components/UserDisplay.js
@@ -2,17 +2,21 @@ import React from "react";
 import PetDisplay from "./PetDisplay";
 
 function UserDisplay({ name, pets, id, onUserDelete, onPetDelete }) {
-  const petElements = pets.map((pet) => (
-    <PetDisplay
-      key={pet.name}
-      id={pet.id}
-      name={pet.name}
-      description={pet.description}
-      user_id={pet.user_id}
-      img_url={pet.img_url}
-      onPetDelete={onPetDelete}
-    ></PetDisplay>
-  ));
+  const petElements = pets ? (
+    pets.map((pet) => (
+      <PetDisplay
+        key={pet.name}
+        id={pet.id}
+        name={pet.name}
+        description={pet.description}
+        user_id={pet.user_id}
+        img_url={pet.img_url}
+        onPetDelete={onPetDelete}
+      ></PetDisplay>
+    ))
+  ) : (
+    <div>Pets load error</div>
+  );
 
   function handleDeleteClick() {
     fetch(`http://localhost:9292/users/${id}`, {
